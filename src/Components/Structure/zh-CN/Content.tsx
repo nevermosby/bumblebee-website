ee6b987d4c09cd19dcdbea82452d9518d611b081
nevermosby <legendarilylwq@gmail.com>
2022-01-11 16:05:10 +0800

    finalize

--- src/Components/Structure/zh-CN/Content.tsx
+++ src/Components/Structure/zh-CN/Content.tsx
@@ -479,10 +479,6 @@ export const Content = () => {
         <Banner>
           <BannerContent>
             <BannerDescription>
-              BumbleBee simplifies building eBPF tools and allows you to
-              package, distribute, and run them anywhere. Just focus on the eBPF
-              portion of your code and BumbleBee automates away the boilerplate,
-              including the userspace code.
               BumbleBee简化了eBPF工具的构建过程，并允许你在任何地方打包、分发和运行它们。
               你只需专注于eBPF代码部分，剩下交给BumbleBee，它可以自动消除样板代码，包括用户空间的代码。
               <BannerSVGs>
@@ -500,7 +496,7 @@ export const Content = () => {
             <BottomRowWording>
               <BottomRowTitle>构建</BottomRowTitle>
               <div>
-                打造"恰到好处"的eBPF工具链是很难的。
+                打造"恰到好处"的eBPF工具链是不易的。
                 BumbleBee将构建过程自动化，让你专注于代码本身。
                 BumbleBee将你的eBPF代码打包成一个OCI标准镜像，这样就可以在你的基础设施中分发它。
               </div>
@@ -530,10 +526,6 @@ export const Content = () => {
             <BottomRowWording className="furtherUpOnMobile">
               <BottomRowTitle>运行</BottomRowTitle>
               <div>
-                With BumbleBee, you focus on your eBPF code and run it anywhere.
-                BumbleBee also builds the userspace code and can expose the eBPF
-                maps as logs, metrics, and histograms. BumbleBee leverages BTF
-                introspection to know what types to display.
                 使用BumbleBee，你可以专注于你的eBPF代码，并在任何地方运行它。
                 BumbleBee不但构建了用户空间代码，而且可以利用eBPF map，展示日志、指标和柱状图信息。
                 BumbleBee也利用BTF格式自审能力，可以知道需要显示哪些数据类型。
